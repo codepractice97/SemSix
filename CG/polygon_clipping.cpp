96b4b25ceafc63c67092afb29d697925cd4f3fe8
Mukul Singh <mukulsingh1248@gmail.com>
2020-02-16 23:52:58 +0530

    CG: Polygon clipping sort of done

--- CG/polygon_clipping.cpp
+++ CG/polygon_clipping.cpp
@@ -1,125 +1,123 @@
 #include <iostream>
 #include <stdio.h>
 #include <graphics.h>
+#include <list>
 
 struct Vertice {
 	int x, y;
-	Vertice(int x, int y){
+	Vertice(int x, int y){ setVertice(x, y); }
+	Vertice(){}
+	void setVertice(int x, int y){
 		this->x = x;
 		this->y = y;
 	}
 };
 
-struct Edge {
-	Vertice* v1;
-	Vertice* v2;
-	Edge(Vertice* v1, Vertice* v2){
-		this->v1 = v1;
-		this->v2 = v2;
-	}
-};
-
-struct Polygon {
-	int vCount;
-	Vertice ** vertices;
-	Polygon(int vCount){
-		this->vCount = vCount;
-		vertices = new Vertice*[vCount];
-	}
-	void setiVertice(int i, Vertice * vertice){
-		vertices[i] = vertice;
-	}
-	Vertice * getiVertice(int i){
-		return vertices[i];
-	}
-	int getvCount(){
-		return vCount;
-	}
-};
-
-Vertice* inputVertice(){
+Vertice inputVertice(){
 	int x, y;
 	std::cout << "X: ";
 	std::cin >> x;
 	std::cout << "Y: ";
 	std::cin >> y;
-	return new Vertice(x, y);
+	return Vertice(x, y);
 }
 
-Vertice inputVMin(){
-	std::cout << "Enter top left vertice of clipping window" << std::endl;
-	return *inputVertice();
-}
-
-Vertice inputVMax(){
-	std::cout << "Enter bottom right vertice of clipping window" << std::endl;
-	return *inputVertice();
-}
-
-Polygon inputPolygon(){
+std::list<Vertice> inputPolygon(){
 	std::cout << "Enter number of vertices in polygon: ";
 	int vCount;
 	std::cin >> vCount;
-	Polygon polygon(vCount);
+	std::list<Vertice> polyVertices;
 	for (int i = 0; i < vCount; i++){
 		std::cout << "Enter vertice " << i + 1 << std::endl;
-		polygon.setiVertice(i, inputVertice());
+		polyVertices.push_back(inputVertice());
 	}
-	return polygon;
+	return polyVertices;
 }
 
-void SutherlandHodgmanPolygonClipping(Polygon &polygon, Edge &clipEdge){
-	int vCount = polygon.getvCount();
-	Vertice* p = polygon.getiVertice(vCount - 1), *q;
-	for (int i = 0; i < vCount; i++){
-		q = polygon.getiVertice(i);
-		if (inside(p, clipEdge)){
-			if (inside(q, clipEdge)){
+void drawPolygon(std::list<Vertice> &polygon){
+	std::list<Vertice>::iterator it = polygon.begin();
+	std::list<Vertice>::iterator rit = polygon.end(); rit--;
+	for (; it != rit ;){
+		Vertice v1 = *it, v2 = *(++it);
+		line(v1.x, v1.y, v2.x, v2.y);
+	}
+	Vertice v1 = *rit, v2 = *polygon.begin();
+	line(v1.x, v1.y, v2.x, v2.y);
+}
 
-			} else {
+void draw(std::list<Vertice> &window, std::list<Vertice> &polyVertices){
+	setcolor(WHITE);
+	drawPolygon(window);
+	setcolor(MAGENTA);
+	drawPolygon(polyVertices);
+}
 
+bool inside(Vertice r, Vertice s, Vertice v){
+	int x1 = r.x, y1 = r.y, x2 = s.x, y2 = s.y;
+	int x = v.x, y = v.y;
+	int d = (y - y1) * (x2 - x1) - (x - x1) * (y2 - y1);
+	std::cout << d << std::endl;
+	// d > 0 if point on right side of edge
+	std::cout << ((d >= 0) ? "True" : "False");
+	return(d >= 0);
+}
+
+Vertice intersection(Vertice p, Vertice q, Vertice r, Vertice s){
+	int x1 = p.x, y1 = p.y, x2 = q.x, y2 = q.y;
+	int x3 = r.x, y3 = r.y, x4 = s.x, y4 = s.y;
+
+	int x = ((x1*y2 - y1*x2)*(x3 - x4) - (x1 - x2)*(x3*y4 - y3*x4)) /
+			((x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4));
+	int y = ((x1*y2 - y1*x2)*(y3 - y4) - (y1 - y2)*(x3*y4 - y3*x4)) /
+			((x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4));
+
+	return Vertice(x, y);
+}
+
+void SutherlandHodgmanPolygonClipping(Vertice r, Vertice s, std::list<Vertice> &polyVertices){
+	std::list<Vertice>::iterator p = polyVertices.end(); p--;
+	for (std::list<Vertice>::iterator q = polyVertices.begin(); q != polyVertices.end(); ++q){
+		if (inside(r, s , *p)){
+			if (!inside(r, s, *q)){ // Edge intersecting
+				polyVertices.insert(q, intersection(*p, *q, r, s));
 			}
 		} else {
-			if (inside(q, clipEdge)){
-
+			if (inside(r, s, *q)){ // Edge intersecting
+				if (p != (--polyVertices.end())){
+					polyVertices.insert(q, intersection(*p, *q, r, s));
+					polyVertices.erase(p);
+				} else
+					polyVertices.push_back(intersection(*p, *q, r, s));
 			}
 		}
 		p = q;
 	}
+	std::cout << "HELL" << std::endl;
 }
 
-void doCLipping(Vertice &vmin, Vertice &vmax, Polygon &polygon){
-	Edge clipEdge1(new Vertice(vmax.x, vmin.y), new Vertice(vmax.x, vmax.y));
-	SutherlandHodgmanPolygonClipping(polygon, clipEdge1);
-}
-
-void draw(Vertice &vmin, Vertice &vmax, Polygon &polygon){
-	rectangle(vmin.x, vmin.y, vmax.x, vmax.y);
-	int vCount = polygon.getvCount();
-	for (int i = 0; i < vCount - 1; i++){
-		line(polygon.getiVertice(i)->x,
-			polygon.getiVertice(i)->y,
-			polygon.getiVertice(i+1)->x,
-			polygon.getiVertice(i+1)->y);
+void doCLipping(std::list<Vertice>& window, std::list<Vertice> &polyVertices){
+	Vertice r = window.back(), s;
+	for (Vertice & s: window){
+		SutherlandHodgmanPolygonClipping(r, s, polyVertices);
+		r = s;
 	}
-	line(polygon.getiVertice(vCount - 1)->x,
-		polygon.getiVertice(vCount - 1)->y,
-		polygon.getiVertice(0)->x,
-		polygon.getiVertice(0)->y);
 }
 
 int main(){
-	Vertice vmin = inputVMin();
-	Vertice vmax = inputVMax();
-	Polygon polygon = inputPolygon();
+	std::cout << "Clipping window" << std::endl;
+	std::list<Vertice> window = inputPolygon();
+	std::cout << "Polygon to be clipped" << std::endl;
+	std::list<Vertice> polyVertices = inputPolygon();
 
 	int gd = DETECT, gm;
 	initgraph(&gd, &gm, NULL);
 
-	draw(vmin, vmax, polygon);
+	draw(window, polyVertices);
 	getch();
-	doCLipping(vmin, vmax, polygon);
-
+	doCLipping(window, polyVertices);
+	cleardevice();
+	draw(window, polyVertices);
+	getch();
 	closegraph();
 
 	return 0;
