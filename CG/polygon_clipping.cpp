03322cb450ab54409b1dc23778521321dfc23cfb
Mukul Singh <mukulsingh1248@gmail.com>
2020-02-23 04:09:23 +0530

    CG: Polygon Clipping Done

--- CG/polygon_clipping.cpp
+++ CG/polygon_clipping.cpp
@@ -5,12 +5,10 @@
 
 struct Vertice {
 	int x, y;
-	Vertice(int x, int y){ setVertice(x, y); }
-	Vertice(){}
-	void setVertice(int x, int y){
+	Vertice(int x, int y){
 		this->x = x;
 		this->y = y;
-	}
+	 }
 };
 
 Vertice inputVertice(){
@@ -20,6 +18,16 @@ Vertice inputVertice(){
 	std::cout << "Y: ";
 	std::cin >> y;
 	return Vertice(x, y);
+}
+
+Vertice inputVMin(){
+	std::cout << "Enter top left vertice of clipping window" << std::endl;
+	return inputVertice();
+}
+
+Vertice inputVMax(){
+	std::cout << "Enter bottom right vertice of clipping window" << std::endl;
+	return inputVertice();
 }
 
 std::list<Vertice> inputPolygon(){
@@ -45,78 +53,137 @@ void drawPolygon(std::list<Vertice> &polygon){
 	line(v1.x, v1.y, v2.x, v2.y);
 }
 
-void draw(std::list<Vertice> &window, std::list<Vertice> &polyVertices){
+void draw(Vertice vmin, Vertice vmax, std::list<Vertice> &polyVertices){
 	setcolor(WHITE);
-	drawPolygon(window);
+	line(vmin.x, vmin.y, vmax.x, vmin.y);
+	line(vmax.x, vmin.y, vmax.x, vmax.y);
+	line(vmax.x, vmax.y, vmin.x, vmax.y);
+	line(vmin.x, vmax.y, vmin.x, vmin.y);
 	setcolor(MAGENTA);
 	drawPolygon(polyVertices);
 }
 
-bool inside(Vertice r, Vertice s, Vertice v){
-	int x1 = r.x, y1 = r.y, x2 = s.x, y2 = s.y;
-	int x = v.x, y = v.y;
-	int d = (y - y1) * (x2 - x1) - (x - x1) * (y2 - y1);
-	std::cout << d << std::endl;
-	// d > 0 if point on right side of edge
-	std::cout << ((d >= 0) ? "True" : "False");
-	return(d >= 0);
-}
-
-Vertice intersection(Vertice p, Vertice q, Vertice r, Vertice s){
-	int x1 = p.x, y1 = p.y, x2 = q.x, y2 = q.y;
-	int x3 = r.x, y3 = r.y, x4 = s.x, y4 = s.y;
-
-	int x = ((x1*y2 - y1*x2)*(x3 - x4) - (x1 - x2)*(x3*y4 - y3*x4)) /
-			((x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4));
-	int y = ((x1*y2 - y1*x2)*(y3 - y4) - (y1 - y2)*(x3*y4 - y3*x4)) /
-			((x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4));
-
-	return Vertice(x, y);
-}
-
-void SutherlandHodgmanPolygonClipping(Vertice r, Vertice s, std::list<Vertice> &polyVertices){
+std::list<Vertice> clipTop(int bound, std::list<Vertice> &polyVertices){
+	std::list<Vertice> clippedPoly;
 	std::list<Vertice>::iterator p = polyVertices.end(); p--;
 	for (std::list<Vertice>::iterator q = polyVertices.begin(); q != polyVertices.end(); ++q){
-		if (inside(r, s , *p)){
-			if (!inside(r, s, *q)){ // Edge intersecting
-				polyVertices.insert(q, intersection(*p, *q, r, s));
+		if (q->y >= bound){ // q inside
+			if (p->y >= bound){ // p inside
+				clippedPoly.push_back(*q);
+			} else { // Edge intersecting
+				int x = p->x + (q->x - p->x)*(bound - p->y)/(q->y - p->y);
+				int y = bound;
+				clippedPoly.push_back(Vertice(x,y));
+				clippedPoly.push_back(*q);
 			}
 		} else {
-			if (inside(r, s, *q)){ // Edge intersecting
-				if (p != (--polyVertices.end())){
-					polyVertices.insert(q, intersection(*p, *q, r, s));
-					polyVertices.erase(p);
-				} else
-					polyVertices.push_back(intersection(*p, *q, r, s));
+			if (p->y >= bound){ // p inside, Edge intersecting
+				int x = p->x + (q->x - p->x)*(bound - p->y)/(q->y - p->y);
+				int y = bound;
+				clippedPoly.push_back(Vertice(x,y));
 			}
 		}
 		p = q;
 	}
-	std::cout << "HELL" << std::endl;
+	return clippedPoly;
 }
 
-void doCLipping(std::list<Vertice>& window, std::list<Vertice> &polyVertices){
-	Vertice r = window.back(), s;
-	for (Vertice & s: window){
-		SutherlandHodgmanPolygonClipping(r, s, polyVertices);
-		r = s;
+std::list<Vertice> clipRight(int bound, std::list<Vertice> &polyVertices){
+	std::list<Vertice> clippedPoly;
+	std::list<Vertice>::iterator p = polyVertices.end(); p--;
+	for (std::list<Vertice>::iterator q = polyVertices.begin(); q != polyVertices.end(); ++q){
+		if (q->x <= bound){ // q inside
+			if (p->x <= bound){ // p inside
+				clippedPoly.push_back(*q);
+			} else { // Edge intersecting
+				int x = bound;
+				int y = p->y + (q->y - p->y)*(bound - p->x)/(q->x - p->x);
+				clippedPoly.push_back(Vertice(x,y));
+				clippedPoly.push_back(*q);
+			}
+		} else {
+			if (p->x <= bound){ // p inside, Edge intersecting
+				int x = bound;
+				int y = p->y + (q->y - p->y)*(bound - p->x)/(q->x - p->x);
+				clippedPoly.push_back(Vertice(x,y));
+			}
+		}
+		p = q;
 	}
+	return clippedPoly;
+}
+
+std::list<Vertice> clipBottom(int bound, std::list<Vertice> &polyVertices){
+	std::list<Vertice> clippedPoly;
+	std::list<Vertice>::iterator p = polyVertices.end(); p--;
+	for (std::list<Vertice>::iterator q = polyVertices.begin(); q != polyVertices.end(); ++q){
+		if (q->y <= bound){ // q inside
+			if (p->y <= bound){ // p inside
+				clippedPoly.push_back(*q);
+			} else { // Edge intersecting
+				int x = p->x + (q->x - p->x)*(bound - p->y)/(q->y - p->y);
+				int y = bound;
+				clippedPoly.push_back(Vertice(x,y));
+				clippedPoly.push_back(*q);
+			}
+		} else {
+			if (p->y <= bound){ // p inside, Edge intersecting
+				int x = p->x + (q->x - p->x)*(bound - p->y)/(q->y - p->y);
+				int y = bound;
+				clippedPoly.push_back(Vertice(x,y));
+			}
+		}
+		p = q;
+	}
+	return clippedPoly;
+}
+
+std::list<Vertice> clipLeft(int bound, std::list<Vertice> &polyVertices){
+	std::list<Vertice> clippedPoly;
+	std::list<Vertice>::iterator p = polyVertices.end(); p--;
+	for (std::list<Vertice>::iterator q = polyVertices.begin(); q != polyVertices.end(); ++q){
+		if (q->x >= bound){ // q inside
+			if (p->x >= bound){ // p inside
+				clippedPoly.push_back(*q);
+			} else { // Edge intersecting
+				int x = bound;
+				int y = p->y + (q->y - p->y)*(bound - p->x)/(q->x - p->x);
+				clippedPoly.push_back(Vertice(x,y));
+				clippedPoly.push_back(*q);
+			}
+		} else {
+			if (p->x >= bound){ // p inside, Edge intersecting
+				int x = bound;
+				int y = p->y + (q->y - p->y)*(bound - p->x)/(q->x - p->x);
+				clippedPoly.push_back(Vertice(x,y));
+			}
+		}
+		p = q;
+	}
+	return clippedPoly;
+}
+
+std::list<Vertice> doCLipping(Vertice &vmin, Vertice &vmax, std::list<Vertice> &polyVertices){
+	// clipRight(Vertice(vmax.x, vmin.y), Vertice(vmax.x, vmax.y), polyVertices);
+	std::list<Vertice> clippedPoly = clipTop(vmin.y, polyVertices);
+	clippedPoly = clipRight(vmax.x, clippedPoly);
+	clippedPoly = clipBottom(vmax.y, clippedPoly);
+	return clipLeft(vmin.x, clippedPoly);
 }
 
 int main(){
-	std::cout << "Clipping window" << std::endl;
-	std::list<Vertice> window = inputPolygon();
-	std::cout << "Polygon to be clipped" << std::endl;
+	Vertice vmin = inputVMin();
+	Vertice vmax = inputVMax();
 	std::list<Vertice> polyVertices = inputPolygon();
 
 	int gd = DETECT, gm;
 	initgraph(&gd, &gm, NULL);
 
-	draw(window, polyVertices);
+	draw(vmin, vmax, polyVertices);
 	getch();
-	doCLipping(window, polyVertices);
+	std::list<Vertice> clippedPoly = doCLipping(vmin, vmax, polyVertices);
 	cleardevice();
-	draw(window, polyVertices);
+	draw(vmin, vmax, clippedPoly);
 	getch();
 	closegraph();
 
